25e95a915854496ae55fd7da0cfd45954fb01207
Ralf Uhlig <ralf@uhlig-online.de>
2022-03-02 22:20:05 +0100

    Added description for deployment on a linux server. Added configurations for systemd service and nginx as proxy.

--- app/main.py
+++ app/main.py
@@ -263,3 +263,138 @@ def root():
 #      In a console: heroku run "alembic upgrade head"
 #   Restart the heroku app: In a console: heroku ps:restart
 #   Show the error log: In a console: heroku logs --tail
+#
+# Deploy on Ubuntu VM (here it's done with RasbianOS 11)
+# https://www.youtube.com/watch?v=0sOvCWFmrtA&t=43504s
+# Install RasbianOS 11 on Raspberry Pi 4
+# Update packages
+# https://www.youtube.com/watch?v=0sOvCWFmrtA&t=43684s
+#   sudo apt update
+#   sudo apt
+# Install git
+#    sudo apt install git
+# Install Python
+# https://www.youtube.com/watch?v=0sOvCWFmrtA&t=43847s
+#   sudo apt install python3-pip
+# Install virtualenv
+#   sudo pip install virtualenv
+# Install Postgres
+# https://www.youtube.com/watch?v=0sOvCWFmrtA&t=43941s
+#   sudo apt install postgresql postgresql-contrib
+# Configure postgres
+# https://www.youtube.com/watch?v=0sOvCWFmrtA&list=RDCMUC8butISFwT-Wl7EV0hUK0BQ&index=1&t=44248s
+# By default postgres is using peer authentication (sometimes named integrated authentication).
+# That means, that the connection is always done with the user that is logged on the machine.
+# With the installation of postgres, a system user postgres is created. So the login has to be
+# with this user postgres.
+#   sudo su - postgres
+# Connect to the default database postgres and default user postgres with psql:
+#   psql -U postgres
+# Set a password for database user postgres (here postgres):
+#   \password postgres
+# Exit from psql:
+#   \q
+# Leave user postgres:
+#   exit
+# Change the postgres configuration to allow access not only from localhost:
+#   sudo nano /etc/postgresql/13/main/postgresql.conf
+#     Under listen_addresses = 'localhost' add
+#     listen_addresses = '*'
+# Change the postgres client authentication to allow access with a given user
+# and from other computers than localhost:
+#   sudo nano /etc/postgresql/13/main/pg_hba.conf
+#     In line: local   all             postgres                                peer
+#       Change peer by md5
+#     In line: local   all             all                                     peer
+#       Change peer by md5
+#     In line: host    replication     all             127.0.0.1/32            md5
+#       Change 127.0.0.1/32 by 0.0.0.0/0
+#     In line: host    all             all             ::1/128                 md5
+#       Change ::1/128 by ::/0
+# Restart postgres
+#   sudo systemctl restart postgresql
+# Add a new user
+# https://www.youtube.com/watch?v=0sOvCWFmrtA&list=RDCMUC8butISFwT-Wl7EV0hUK0BQ&index=1&t=44690s
+#   Create a user: sudo adduser <username>
+#   Give a passwort
+#   Allow the user to use sudo: sudo usermog -aG sudo <username>
+#   Login with this user
+# Create a directory for our project
+#   mkdir FastAPICourse
+#   cd FastAPICourse
+# Create a virtual Python environment
+# https://www.youtube.com/watch?v=0sOvCWFmrtA&list=RDCMUC8butISFwT-Wl7EV0hUK0BQ&index=1&t=44690s
+#   Create the environment: virtualenv venv
+#   Activate the environment: source venv/bin/activate
+#   Leave an environment: deactivate
+# Create a folder for the source code
+#   mkdir src
+#   cd src
+# Get the project from github
+# https://youtu.be/0sOvCWFmrtA?list=RDCMUC8butISFwT-Wl7EV0hUK0BQ&t=44986
+#   Clone the project from github without creating another directory
+#   with the name of the project:
+#     git clone https://github.com/RalfUhlig/fastapicourse.git .
+#   Install all requirements
+#     pip install -r requirements.txt
+#   Try to start the app
+# Set environment variables
+# https://www.youtube.com/watch?v=0sOvCWFmrtA&list=RDCMUC8butISFwT-Wl7EV0hUK0BQ&index=1&t=45246s
+#   Set an environment variable: export name=value
+#   Show all environment variables: printenv
+#   Delete an environment variable: unset name
+#   Create an empty file named .env: touch .env
+#   Fill with necessary variables.
+#   Set all variables as exported: set -o allexport; source ~/.env; set +o allexport
+#   One way to automatically load the environment variables after booting is to
+#   put this command into file .profile at the bottom.
+# Setup te database
+# https://www.youtube.com/watch?v=0sOvCWFmrtA&list=RDCMUC8butISFwT-Wl7EV0hUK0BQ&index=1&t=45744s
+#   Create the database FastAPICourse
+#   Note: In postgres there is no way to see all databases. You always connect to one database
+#         and work with it. The database is shown as "public".
+#   Create the database structure: alembic upgrade head
+# Start the application again, listening on any ip address.
+# https://www.youtube.com/watch?v=0sOvCWFmrtA&list=RDCMUC8butISFwT-Wl7EV0hUK0BQ&index=1&t=45957s
+#   uvicorn --host 0.0.0.0 app.main:app
+# Use process manager to supervise the app.
+# https://www.youtube.com/watch?v=0sOvCWFmrtA&list=RDCMUC8butISFwT-Wl7EV0hUK0BQ&index=1&t=46452s
+#   Install gunicorn and its requirements:
+#     pip install gunicorn
+#     pip install httptools
+#     pip install uvloop
+#   Update requirements.txt
+#   gunicorn allows more than one worker (option -w or --workers)
+#   Start app with gunicorn with 4 uvicorn workers listing at any ip address on port 8000:
+#     gunicorn --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 app.main:app
+# Run the app as systemd service
+# https://www.youtube.com/watch?v=0sOvCWFmrtA&list=RDCMUC8butISFwT-Wl7EV0hUK0BQ&index=1&t=46452s
+#   Service file: gunicorn.service
+#     Description: Description of the service.
+#     After: When to start.Here after network is up.
+#     User: User to run the service.
+#     Group: Group of the user (see /etc/group).
+#     WorkingDirectory: Directory the service is running in.
+#     Environment: Path to the bin folder for the Python environment.
+#     ExecStart: Path to the command to run the service.
+#     WantedBy: Running the service when the system starts in multi user mode.
+#   Copy gunicorn.service to /etc/systemd/system (as root) as FastAPICourse.service.
+#   or edit it by: sudo nano /etc/systemd/system/FastAPICourse.service
+#   Note: After editing a service file: sudo systemctl daemon-reload
+#   Check the service file: sudo systemctl status FastAPICourse
+#   Start the service: sudo systemctl start FastAPICourse
+#   Enable automatic restart after reboot: sudo systemctl enable FastAPICourse
+# Using NGINX as a high performance proxy
+# https://www.youtube.com/watch?v=0sOvCWFmrtA&list=RDCMUC8butISFwT-Wl7EV0hUK0BQ&index=1&t=47085s
+#     High performance webservers can act as a proxy.
+#     Can handle SSL termination.
+#     HTTPS request ------> NGINX -----> HTTP -----> Gunicorn workers
+#     Gunicorn can handle HTTPS too, but it is not optimized for that as NGINX is.
+#   Install NGINX: sudo apt install nginx
+#   Start NGINX: sudo systemctl start nginx
+#   Configuration of the default page: /etc/nginx/sites-available/default
+#   Copy gunicorn.nginx to /etc/nginx/sites-available/FastAPICourse
+#   Enable the new site: sudo ln -s /etc/nginx/sites-available/FastAPICourse /etc/nginx/sites-enabled/FastAPICourse
+#   Disable the default site: sudo rm /etc/nginx/sites-enabled/default
+#   Restart nginx: sudo systemctl restart nginx
+#   App now available by http://webservicebox
